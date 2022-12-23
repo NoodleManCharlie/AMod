e921d756f202332611e2dbde9756938c042dc7f4
Charlie Carter <cmcarter1908@gmail.com>
2022-12-23 15:08:46 -0500

    Beanchest shpuld work fully

--- src/main/java/net/fabricmc/example/blocks/BeanChestEntity.java
+++ src/main/java/net/fabricmc/example/blocks/BeanChestEntity.java
@@ -2,45 +2,41 @@ package net.fabricmc.example.blocks;
 
 import org.jetbrains.annotations.Nullable;
 
+import net.fabricmc.example.ImplementedInventory;
 import net.fabricmc.example.item.Registery;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 
-public class BeanChestEntity extends BlockEntity {
-    private int number = 7;
+public class BeanChestEntity extends BlockEntity implements ImplementedInventory {
+    private final DefaultedList<ItemStack> items = DefaultedList.ofSize(90, ItemStack.EMPTY);
 
     public BeanChestEntity(BlockPos pos, BlockState state) {
         super(Registery.BeanChestEntitie, pos, state);
     }
 
-    public void writeNbt(NbtCompound nbt) {
-        // Save the current value of the number to the nbt
-        nbt.putInt("number", number);
- 
-        super.writeNbt(nbt);
-        
+    @Override
+    public DefaultedList<ItemStack> getItems() {
+        return items;
     }
 
     @Override
     public void readNbt(NbtCompound nbt) {
         super.readNbt(nbt);
+        Inventories.readNbt(nbt, items);
+    }
  
-        number = nbt.getInt("number");
-    }
-
-    /*@Nullable
     @Override
-    public Packet<ClientPlayPacketListener> toUpdatePacket() {
-      return BlockEntityUpdateS2CPacket.create(this);
+    public void writeNbt(NbtCompound nbt) {
+        
+        Inventories.writeNbt(nbt, items);
+        super.writeNbt(nbt);
     }
-   
-    @Override
-    public NbtCompound toInitialChunkDataNbt() {
-      return createNbt();
-    }*/
 }
